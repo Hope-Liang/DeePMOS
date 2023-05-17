44516c30b42a98ef1944048763b903d7716fe1db
Hope-Liang <hope-liang@sjtu.edu.cn>
2023-05-17 12:33:41 +0200

    update

--- dataset.py
+++ dataset.py
@@ -2,10 +2,14 @@ import os
 from tqdm import tqdm
 import librosa
 import numpy as np
+from collections import defaultdict
+import scipy
+import h5py
 import torch
 from torch.utils.data.dataset import Dataset
 from torchvision.transforms import Normalize
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 import pandas as pd
 
 class VCC16Dataset(Dataset):
@@ -53,7 +57,6 @@ class VCC18Dataset(Dataset):
             self.idtable = torch.load(idtable) # a dictionary from judge code to id number, 0 to 269
             for i, judge_i in enumerate(score_csv['JUDGE']):
                 self.scores['JUDGE'][i] = self.idtable[judge_i]
-
         elif not valid:
             self.gen_idtable(idtable) # generate for training data, but actually the same as the one read
             
@@ -81,7 +84,6 @@ class VCC18Dataset(Dataset):
                 self.scores['JUDGE'][i] = self.idtable[judge_i]
         torch.save(self.idtable, idtable_path)
 
-
     def collate_fn(self, samples):
         # wavs may be list of wave or spectrogram, which has shape (time, feature) or (time,)
         wavs, filenames, means, scores, judge_ids = zip(*samples)
@@ -100,7 +102,101 @@ class VCC18Dataset(Dataset):
         judge_ids = torch.LongTensor(judge_ids)
         return output_wavs, filenames, judge_ids, means, scores
 
-def get_dataset(data_path, split, load_all = False, vcc18 = False, idtable = '', valid = False):
+class BVCCDataset(Dataset):
+    def __init__(self, metadata, data_path, idtable='', split="train", valid=False):
+        self.data_path = data_path
+        self.split = split
+
+        # cache features
+        self.features = {}
+
+        # get judge id table and number of judges
+        if os.path.isfile(idtable):
+            self.idtable = torch.load(idtable)
+        elif not valid:
+            self.gen_idtable(metadata, idtable)
+        self.num_judges = len(self.idtable)
+
+        self.metadata = []
+        if self.split == "train":
+            for wav_name, judge_name, avg_score, score in metadata:
+                self.metadata.append([wav_name, avg_score, score, self.idtable[judge_name]])
+        else:
+            for item in metadata:
+                self.metadata.append(item)
+
+            # build system list
+            self.systems = list(set([item[0] for item in metadata]))
+            
+    def __getitem__(self, idx):
+        if self.split == "train":
+            wav_name, avg_score, score, judge_id = self.metadata[idx]
+        else:
+            sys_name, wav_name, avg_score = self.metadata[idx]
+
+        # cache features
+        if wav_name in self.features:
+            mag_sgram = self.features[wav_name]
+        else:
+            h5_path = os.path.join(self.data_path, "bin", wav_name + ".h5")
+            if os.path.isfile(h5_path):
+                data_file = h5py.File(h5_path, 'r')
+                mag_sgram = np.array(data_file['mag_sgram'][:])
+                timestep = mag_sgram.shape[0]
+                mag_sgram = np.reshape(mag_sgram,(timestep, 257))
+            else:
+                wav, _ = librosa.load(os.path.join(self.data_path, "wav", wav_name), sr = 16000)
+                mag_sgram = np.abs(librosa.stft(wav, n_fft = 512, hop_length=256, win_length=512, window=scipy.signal.hamming)).astype(np.float32).T
+            self.features[wav_name] = mag_sgram
+
+        if self.split == "train":
+            return mag_sgram, avg_score, score, judge_id
+        else:
+            return mag_sgram, avg_score, sys_name, wav_name
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def gen_idtable(self, metadata, idtable_path):
+        if idtable_path == '':
+            idtable_path = './idtable.pkl'
+        self.idtable = {}
+        count = 0
+        for _, judge_name, _, _ in metadata:
+            if judge_name not in self.idtable:
+                self.idtable[judge_name] = count
+                count += 1
+        torch.save(self.idtable, idtable_path)
+
+    def collate_fn(self, batch):
+        sorted_batch = sorted(batch, key=lambda x: -x[0].shape[0])
+        bs = len(sorted_batch) # batch_size
+        avg_scores = torch.FloatTensor([sorted_batch[i][1] for i in range(bs)])
+        mag_sgrams = [torch.from_numpy(sorted_batch[i][0]) for i in range(bs)]
+        mag_sgrams_lengths = torch.from_numpy(np.array([mag_sgram.size(0) for mag_sgram in mag_sgrams]))
+        
+        # repetitive padding
+        max_len = mag_sgrams_lengths[0]
+        mag_sgrams_padded = []
+        for mag_sgram in mag_sgrams:
+            this_len = mag_sgram.shape[0]
+            dup_times = max_len // this_len
+            remain = max_len - this_len * dup_times
+            to_dup = [mag_sgram for t in range(dup_times)]
+            to_dup.append(mag_sgram[:remain, :])
+            mag_sgrams_padded.append(torch.Tensor(np.concatenate(to_dup, axis = 0)))
+        mag_sgrams_padded = torch.stack(mag_sgrams_padded, dim = 0)
+
+        if not self.split == "train":
+            sys_names = [sorted_batch[i][2] for i in range(bs)]
+            return mag_sgrams_padded, avg_scores, sys_names
+        else:
+            scores = torch.FloatTensor([sorted_batch[i][2] for i in range(bs)])
+            judge_ids = torch.LongTensor([sorted_batch[i][3] for i in range(bs)])
+            return mag_sgrams_padded, mag_sgrams_lengths, judge_ids, avg_scores, scores
+
+
+def get_dataset(data_path, split, vcc18 = False, bvcc = False, idtable = '', valid = False):
     if vcc18:
         dataframe = pd.read_csv(os.path.join(data_path, f'{split}'), index_col=False)
         wavs = []
@@ -119,6 +215,38 @@ def get_dataset(data_path, split, load_all = False, vcc18 = False, idtable = '',
                 wavs.append(last)
 
         return VCC18Dataset(wav_file=wavs, score_csv = dataframe, idtable = idtable, valid = valid)
+    elif bvcc:
+        names = {"train":"TRAINSET", "valid":"DEVSET", "test":"TESTSET"}
+    
+        metadata = defaultdict(dict)
+        metadata_with_avg = list()
+
+        # read metadata
+        with open(os.path.join(data_path, "sets", names[split]), "r") as f:
+            lines = f.read().splitlines()
+           
+            # line has format <system, wav_name, score, _, judge_name>
+            for line in lines:
+                if line:
+                    parts = line.split(",")
+                    sys_name = parts[0]
+                    wav_name = parts[1]
+                    score = int(parts[2])
+                    judge_name = parts[4]
+                    metadata[sys_name + "|" + wav_name][judge_name] = score
+        
+        # calculate average score
+        for _id, v in metadata.items():
+            sys_name, wav_name = _id.split("|")
+            avg_score = np.mean(np.array(list(v.values())))
+            if split == "train":
+                for judge_name, score in v.items():
+                    metadata_with_avg.append([wav_name, judge_name, avg_score, score])
+            else:
+                # in testing mode, additionally return system name and only average score
+                metadata_with_avg.append([sys_name, wav_name, avg_score])
+
+        return BVCCDataset(metadata_with_avg, data_path, idtable, split, valid = valid)
     return VCC16Dataset(data_path)
 
 def get_dataloader(dataset, batch_size, num_workers, shuffle=True):
--- train.py
+++ train.py
@@ -21,19 +21,21 @@ from EMA import WeightExponentialMovingAverage
 from model import DeePMOS
 
 
-parser = argparse.ArgumentParser(description='Training DeePMOS model.')
+parser = argparse.ArgumentParser(description='Training DeePMOS(gaussian) model.')
 parser.add_argument('--num_epochs', type=int, help='Number of epochs.', default=60)
 parser.add_argument('--lamb_c', type=float, help='Weight of consistency loss lambda_c.', default=1.0)
 parser.add_argument('--lamb_t', type=float, help='Weight of teacher model loss lambda_t.', default=0.0)
 parser.add_argument('--log_valid', type=int, help='Logging valid score each log_valid epochs.', default=1)
 parser.add_argument('--log_epoch', type=int, help='Logging training during a global run.', default=1)
-parser.add_argument('--data_path', type=str, help='Path to data.', default='../testVCC2/')
-parser.add_argument('--id_table', type=str, help='Path to ID of judges.', default='./id_table/')
+parser.add_argument('--dataset', type=str, help='Dataset.', default='vcc2018')
+parser.add_argument('--data_path', type=str, help='Path to data.', default='../VCC2018/testVCC2/')
+parser.add_argument('--id_table', type=str, help='Path to ID of judges.', default='../VCC2018/id_table/')
 parser.add_argument('--save_path', type=str, help='Path to save the model.', default='')
 args = parser.parse_args()
 
 
-def valid(model, 
+def valid(model,
+          dataset,
           dataloader, 
           systems,
           steps,
@@ -44,35 +46,38 @@ def valid(model,
           SRCC_list):
     model.eval()
 
-    mos_predictions = []
+    mos_mus = []
     mos_targets = []
     mos_vars = []
-    mos_predictions_sys = {system:[] for system in systems}
+    mos_mus_sys = {system:[] for system in systems}
     mos_vars_sys = {system:[] for system in systems}
     true_sys_mean_scores = {system:[] for system in systems}
 
     for i, batch in enumerate(tqdm(dataloader, ncols=0, desc=prefix, unit=" step")):
-        wav, filename, _, mos, _ = batch
-        sys_names = list(set([name.split("_")[0] for name in filename])) # system name, e.g. 'D03'
+        if dataset == 'vcc2018':
+            wav, filename, _, mos, _ = batch
+            sys_names = list(set([name.split("_")[0] for name in filename])) # system name, e.g. 'D03'
+        elif dataset == 'bvcc':
+            wav, mos, sys_names = batch
         wav = wav.to(device)
         wav = wav.unsqueeze(1) # shape (batch, 1, seq_len, 257)
 
         with torch.no_grad():
             try:
-                mos_prediction, mos_var = model(speech_spectrum = wav) # shape (batch, seq_len, 1)
-                mos_prediction = mos_prediction.squeeze(-1) # shape (batch, seq_len)
+                mos_mu, mos_var = model(speech_spectrum = wav) # shape (batch, seq_len, 1)
+                mos_mu = mos_mu.squeeze(-1) # shape (batch, seq_len)
                 mos_var = mos_var.squeeze(-1)
-                mos_prediction = torch.mean(mos_prediction, dim = -1) # torch.Size([1])
+                mos_mu = torch.mean(mos_mu, dim = -1) # torch.Size([1])
                 mos_var = torch.mean(mos_var, dim = -1)
 
-                mos_prediction = mos_prediction.cpu().detach().numpy()
+                mos_mu = mos_mu.cpu().detach().numpy()
                 mos_var = mos_var.cpu().detach().numpy()
-                mos_predictions.extend(mos_prediction.tolist())
+                mos_mus.extend(mos_mu.tolist())
                 mos_targets.extend(mos.tolist())
                 mos_vars.extend(mos_var.tolist())
 
                 for j, sys_name in enumerate(sys_names):
-                    mos_predictions_sys[sys_name].append(mos_prediction[j])
+                    mos_mus_sys[sys_name].append(mos_mu[j])
                     mos_vars_sys[sys_name].append(mos_var[j])
                     true_sys_mean_scores[sys_name].append(mos.tolist()[j])
 
@@ -85,45 +90,47 @@ def valid(model,
                 else:
                     raise
     
-    mos_predictions = np.array(mos_predictions)
+    mos_mus = np.array(mos_mus)
     mos_vars = np.array(mos_vars)
     mos_targets = np.array(mos_targets)
 
-    mos_predictions_sys = np.array([np.mean(scores) for scores in mos_predictions_sys.values()])
+    mos_mus_sys = np.array([np.mean(scores) for scores in mos_mus_sys.values()])
     mos_vars_sys = np.array([np.mean(scores)/len(scores) for scores in mos_vars_sys.values()])
     true_sys_mean_scores = np.array([np.mean(scores) for scores in true_sys_mean_scores.values()])
     
-    utt_MSE=np.mean((mos_targets-mos_predictions)**2)
-    utt_LCC=np.corrcoef(mos_targets, mos_predictions)[0][1]
-    utt_SRCC=scipy.stats.spearmanr(mos_targets, mos_predictions)[0]
+    utt_MSE=np.mean((mos_targets-mos_mus)**2)
+    utt_LCC=np.corrcoef(mos_targets, mos_mus)[0][1]
+    utt_SRCC=scipy.stats.spearmanr(mos_targets, mos_mus)[0]
     
-    sys_MSE=np.mean((true_sys_mean_scores-mos_predictions_sys)**2)
-    sys_LCC=np.corrcoef(true_sys_mean_scores, mos_predictions_sys)[0][1]
-    sys_SRCC=scipy.stats.spearmanr(true_sys_mean_scores, mos_predictions_sys)[0]
+    sys_MSE=np.mean((true_sys_mean_scores-mos_mus_sys)**2)
+    sys_LCC=np.corrcoef(true_sys_mean_scores, mos_mus_sys)[0][1]
+    sys_SRCC=scipy.stats.spearmanr(true_sys_mean_scores, mos_mus_sys)[0]
 
     Likelihoods = []
     for i in range(len(mos_targets)):
-        Likelihoods.append(stats.norm.pdf(mos_targets[i], mos_predictions[i], math.sqrt(mos_vars[i])))
-    utt_GML=gmean(Likelihoods) # geometric mean of likelihood
+        Likelihoods.append(stats.norm.pdf(mos_targets[i], mos_mus[i], math.sqrt(mos_vars[i])))
+
+    #utt_GML=gmean(Likelihoods) # geometric mean of likelihood
     utt_AML=np.mean(Likelihoods) # arithemic mean of likelihood
     utt_MoV=np.mean(mos_vars) # mean of variance
     utt_VoV=np.var(mos_vars) # variance of variance
 
-    Likelihoods_sys = []
-    for i in range(len(true_sys_mean_scores)):
-        Likelihoods_sys.append(stats.norm.pdf(true_sys_mean_scores[i], mos_predictions_sys[i], math.sqrt(mos_vars_sys[i])))
-    sys_GML=gmean(Likelihoods_sys)
-    sys_AML=np.mean(Likelihoods_sys)
-    sys_MoV=np.mean(mos_vars_sys)
-    sys_VoV=np.var(mos_vars_sys)
+    # Likelihoods_sys = []
+    # for i in range(len(true_sys_mean_scores)):
+    #     Likelihoods_sys.append(stats.norm.pdf(true_sys_mean_scores[i], mos_predictions_sys[i], math.sqrt(mos_vars_sys[i])))
+    # sys_GML=gmean(Likelihoods_sys)
+    # sys_AML=np.mean(Likelihoods_sys)
+    # sys_MoV=np.mean(mos_vars_sys)
+    # sys_VoV=np.var(mos_vars_sys)
     
     MSE_list.append(utt_MSE)
     LCC_list.append(utt_LCC) 
     SRCC_list.append(utt_SRCC)
+
     print(
         f"\n[{prefix}][{steps}][UTT][ MSE = {utt_MSE:.4f} | LCC = {utt_LCC:.4f} | SRCC = {utt_SRCC:.4f} ] [SYS][ MSE = {sys_MSE:.4f} | LCC = {sys_LCC:.4f} | SRCC = {sys_SRCC:.4f} ]"
     )
-    print(f"[{prefix}][{steps}][UTT][ GML = {utt_GML:.6f} | AML = {utt_AML:.6f} | MoV = {utt_MoV:.6f} | VoV = {utt_VoV:.6f} ] [SYS][ GML = {sys_GML:.6f} | AML = {sys_AML:.6f} | MoV = {sys_MoV:.6f} | VoV = {sys_VoV:.6f} ]")
+    print(f"[{prefix}][{steps}][UTT][ AML = {utt_AML:.6f} | MoV = {utt_MoV:.6f} | VoV = {utt_VoV:.6f} ]" )
 
     model.train()
     return MSE_list, LCC_list, SRCC_list, sys_SRCC
@@ -134,6 +141,7 @@ def train(num_epochs,
           lamb_t,
           log_valid,
           log_epoch,
+          dataset,
           train_set,
           valid_set,
           test_set,
@@ -175,7 +183,7 @@ def train(num_epochs,
         
         for i, batch in enumerate(tqdm(train_loader, ncols=0, desc="Train", unit=" step")):
             try:
-                wavs, filename, _, mos, _ = batch
+                wavs, _, _, mos, _ = batch
                 wavs = wavs.to(device)
                 wavs = wavs.unsqueeze(1) # shape (batch, 1, seq_len, 257[dim feature])
                 mos = mos.to(device) # shape (batch)
@@ -183,6 +191,7 @@ def train(num_epochs,
                 # Stochastic Gradient Noise (SGN)
                 label_noise = torch.randn(mos.size(), device=device) # standard normal distribution
                 mos += 0.1*label_noise
+                mos = torch.clamp(mos, min=1, max=5)
 
                 # Forward
                 mos_mean, mos_var = model(speech_spectrum=wavs) # (batch, seq_len, 1), (batch, seq_len, 1) 
@@ -226,6 +235,7 @@ def train(num_epochs,
 
         if epoch % log_valid == 0:
             MSE_teacher, LCC_teacher, SRCC_teacher, sys_SRCC_teacher = valid(momentum_model,
+                                                                     dataset,
                                                                      valid_loader,
                                                                      valid_set.systems,
                                                                      epoch,
@@ -234,15 +244,6 @@ def train(num_epochs,
                                                                      MSE_teacher,
                                                                      LCC_teacher,
                                                                      SRCC_teacher)
-            # MSE_student, LCC_student, SRCC_student, sys_SRCC_student = valid(model,
-            #                                                         valid_loader,
-            #                                                         valid_set.systems,
-            #                                                         epoch,
-            #                                                         'Valid(student)',
-            #                                                         device,
-            #                                                         MSE_student,
-            #                                                         LCC_student,
-            #                                                         SRCC_student)
 
             if LCC_teacher[-1] > best_LCC_teacher:
                 best_LCC_teacher = LCC_teacher[-1]
@@ -252,35 +253,30 @@ def train(num_epochs,
 
     print('Best model performance test:')
     _, _, _, _ = valid(best_model, test_loader, test_set.systems, epoch, 'Test(best)', device, MSE_teacher, LCC_teacher, SRCC_teacher)
-    _, _, _, _ = valid(momentum_model, test_loader, test_set.systems, epoch, 'Test(last)', device, MSE_teacher, LCC_teacher, SRCC_teacher)
-    return best_model, momentum_model, train_loss, MSE_list, LCC_list, SRCC_list, LCC_teacher
+    return best_model, train_loss, MSE_list, LCC_list, SRCC_list, LCC_teacher
 
 def main():
     data_path = args.data_path
     id_table = args.id_table
-    train_set = get_dataset(data_path,
-                            "training_data.csv",
-                            vcc18=True,
-                            idtable=os.path.join(id_table, 'idtable.pkl'))
-    valid_set = get_dataset(data_path,
-                            "valid_data.csv",
-                            vcc18=True,
-                            valid=True,
-                            idtable=os.path.join(id_table, 'idtable.pkl'))
-    test_set = get_dataset(data_path,
-                           "testing_data.csv",
-                           vcc18=True,
-                           valid=True,
-                           idtable=os.path.join(id_table, 'idtable.pkl'))
+    dataset = args.dataset
+
+    if dataset == 'vcc2018':
+        train_set = get_dataset(data_path, "training_data.csv", vcc18=True, idtable=os.path.join(id_table, 'idtable.pkl'))
+        valid_set = get_dataset(data_path, "valid_data.csv", vcc18=True, valid=True, idtable=os.path.join(id_table, 'idtable.pkl'))
+        test_set = get_dataset(data_path, "testing_data.csv", vcc18=True, valid=True, idtable=os.path.join(id_table, 'idtable.pkl'))
+    elif dataset == 'bvcc':
+        train_set = get_dataset(data_path, "train", bvcc=True, idtable=os.path.join(id_table, 'idtable.pkl'))
+        valid_set = get_dataset(data_path, "valid", bvcc=True, valid=True, idtable=os.path.join(id_table, 'idtable.pkl'))
+        test_set = get_dataset(data_path, "test", bvcc=True, valid=True, idtable=os.path.join(id_table, 'idtable.pkl'))
+
     train_loader = get_dataloader(train_set, batch_size=64, num_workers=1)
     valid_loader = get_dataloader(valid_set, batch_size=1, num_workers=1)
     test_loader = get_dataloader(test_set, batch_size=1, num_workers=1)
     
     best_model, last_model, train_loss, MSE_list, LCC_list, SRCC_list, LCC_teacher = train(
-        args.num_epochs, args.lamb_c, args.lamb_t, args.log_valid, args.log_epoch, train_set,
-        valid_set, test_set, train_loader, valid_loader, test_loader)
+        args.num_epochs, args.lamb_c, args.lamb_t, args.log_valid, args.log_epoch, 
+        dataset, train_set, valid_set, test_set, train_loader, valid_loader, test_loader)
 
     torch.save(best_model, args.save_path+'best.pt')
-    torch.save(last_model, args.save_path+'last.pt')
 
 main()
